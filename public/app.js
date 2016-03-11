5efd3ae2bd84115f1d96d9b08b3c9ff6f05747fe
chriscampbell32 <chriscampbell32@yahoo.com>
2016-03-11 10:18:57 -0500

    sort buttons working in browserside js

--- public/app.js
+++ public/app.js
@@ -1,27 +1,28 @@
 $(document).ready(function(){
         $.getJSON("/all", function(data){
             for(var i = 0; i < data.length; i++){
-                $("#results").append("<tr><td>" + data[i].name + "</td><td>" + data[i].numlegs + "</td><td>" + data[i].class + "</td><td>" + data[i].avgWeight + "</td><td>" + data[i].whatIwouldReallyCallIt + "</td></tr>" );
+                $("#results tbody").append("<tr><td>" + data[i].name + "</td><td>" + data[i].numlegs + "</td><td>" + data[i].class + "</td><td>" + data[i].avgWeight + "</td><td>" + data[i].whatIwouldReallyCallIt + "</td></tr>" );
             }
         });
 
 
-    $("#weightsort").on("click", function(){
+    $("button.sort").on("click", function(){
         // get and log to the console data from the /weight endpoint
-        $.getJSON("/weight", function(data){
-            $("#results").empty();
+        $.getJSON("/" + $(this).val(), function(data){
+            $("#results tbody").empty();
             for(var i = 0; i < data.length; i++){
-               $("results").append
+               var newTr = "<tr>";
+                newTr += "<td>" + data[i].name + "</td>";
+                newTr += "<td>" + data[i].numlegs + "</td>";
+                newTr += "<td>" + data[i].class + "</td>";
+                newTr += "<td>" + data[i].avgWeight + "</td>";
+                newTr += "<td>" + data[i].whatIwouldReallyCallIt + "</td>";
+                newTr += "</tr>";
+                $("#results tbody").append(newTr);
             }
 
         });
         console.log("weight button clicked");
     });
 
-    $("#namesort").on("click", function(){
-        $.getJSON("/name", function(data){
-            console.log(data);
-        });
-        console.log("name button clicked");
-    });
 })
